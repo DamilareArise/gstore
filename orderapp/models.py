570f8744e6accc490946aeed1bcab063086ca0df
Arise Damilare <arisedamilare5@gmail.com>
2026-08-12 15:55:28 +0100

    feat: Add order and checkout functionality; implement order management and templates for user orders

--- orderapp/models.py
+++ orderapp/models.py
@@ -16,4 +16,31 @@ class CartItem(models.Model):
     quantity = models.PositiveIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Order(models.Model):
+    payment_status_choices = [
+        ('pending', 'Pending'),
+        ('completed', 'Completed'),
+        ('failed', 'Failed'),
+    ]
     
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="order")
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)  
+    payment_status = models.CharField(max_length=20, choices=payment_status_choices, default='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="order_items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    item_price = models.DecimalField(max_digits=10, decimal_places=2)  
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    @property
+    def get_total_price(self):
+        return self.quantity * self.item_price
